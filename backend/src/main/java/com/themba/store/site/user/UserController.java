8aae796a80a42288f350903ba0809331a243023e
Themba G Chauke <GoodnessThembaC@gmail.com>
2024-05-21 13:31:17 +0200

    Testing Routes

--- backend/src/main/java/com/themba/store/site/user/UserController.java
+++ backend/src/main/java/com/themba/store/site/user/UserController.java
@@ -1,19 +1,25 @@
 package com.themba.store.site.user;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.themba.store.site.auth.AuthenticationResponse;
+import com.themba.store.site.auth.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/register")
 public class UserController {
-    @GetMapping("/testing")
+    @GetMapping
     public String testing(){
         return "Rest Controller is working";
     }
 
 
-
-
-
+    @GetMapping("/testing")
+    public ResponseEntity<String> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok("Testing");
+    }
 
 }
